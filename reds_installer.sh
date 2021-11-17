9ac1e5a239e7a66d14137f525c0f7e0fb2f78775
armpdq <pdqcontacts@gmail.com>
2021-11-17 13:15:23 +0400

    Update reds_installer.sh

--- reds_installer.sh
+++ reds_installer.sh
@@ -1,4 +1,6 @@
 #!/bin/ash
+wget https://github.com/armpdq/configs/raw/master/wrt.tar.gz -O /tmp/wrt.tar.gz
+sysupgrade -v --restore-backup /tmp/wrt.tar.gz
 opkg update
 opkg install redsocks
 wget https://raw.githubusercontent.com/armpdq/configs/master/proxy_vpn -O /etc/init.d/proxy_vpn
@@ -13,3 +15,5 @@ sed -i "s/REMOTEIP/$1/g" /etc/redsocks.conf
 sed -i "s/REMOTEPORT/$2/g" /etc/redsocks.conf
 sed -i "s/REMOTELOGIN/$3/g" /etc/redsocks.conf
 sed -i "s/REMOTEPASS/$4/g" /etc/redsocks.conf
+sed -i "s/nameOFhost/$5/g" /etc/config/system
+sed -i "s/WIFIname/$6/g" /etc/config/wireless
